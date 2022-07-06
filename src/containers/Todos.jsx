bf6b07b95366923e8357a9ac7d40a5ebdaccbbc8
Matt Biggers <matt.biggers@workday.com>
2022-07-05 21:59:57 -0700

    containers Todos functiontional

--- src/containers/Todos.jsx
+++ src/containers/Todos.jsx
@@ -1,68 +1,26 @@
 import React, { useState, useEffect } from "react";
-import TodoModel from "../models/Todo";
 import Todos from "../components/Todos"
-import CreateToDoForm from "../components/CreateTodoForm";
 
 const TodosContainer = () => {
 
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    console.log('useEffect console.log hit')
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    handleFetchAllTodos();
   }, [])
 
-  const fetchData = () => {
-    TodoModel.all()
-    .then((res) => {
-      const data = res.data.todos;
-      setTodos(data);
-    });
-  };
 
-  // const createTodo = (todo) => {
-  //   let newTodo = {
-  //       body: todo,
-  //       completed: false,
-  //   };
-  //   TodoModel.create(newTodo)
-  //   .then((res) => {
-  //       let todos = this.state.todos;
-  //       todos.push(res.data);
-  //       this.setState({ todos: todos });
-  //   });
-  // };
+  const handleFetchAllTodos = () => {
+    fetch(`https://super-crud.herokuapp.com/todos`)
+    .then(response => response.json())
+    .then(jsonData => setTodos(jsonData))
+    .catch(err => console.log(err));
+  }
 
-  // const deleteTodo = (todo) => {
-  //   TodoModel.delete(todo).then((res) => {
-  //     let todos = this.state.todos.filter((todo) => {
-  //       return todo._id !== res.data._id
-  //     });
-  //     this.setState({todos});
-  //   })
-  // }
-
-  // const updateTodo = todo => {
-  //   const isUpdatedtodo = t => {
-  //     return t._id === todo._id;
-  //   };
-  //   TodoModel.udpate(todo)
-  //   .then((res) => {
-  //     let todos = this.state.todos;
-  //     todos.find(isUpdatedtodo).body = todo.body;
-  //     this.setState({ todos: todos })
-  //   });
-  // };
-
-  console.log('TodoContainer.js')
+  console.log('/containers/TodoContainer.js')
   return (
     <div className = "todosContainer">
-      {/* <CreateToDoForm createToDo={this.createToDo} /> */}
-      <Todos todos={todos}
-        // updateTodo={this.updateTodo}
-        // deleteTodo={this.deleteTodo}
-      />
+      <Todos todos={todos} />
     </div>  
   );
 };
