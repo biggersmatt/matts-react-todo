6a5d7c28ace6d57da854df253dc20db0c73bd3d6
Matt Biggers <matt.biggers@workday.com>
2022-07-05 20:43:28 -0700

    Intiallizing Git

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
-import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById("root")
-)
+// CSS
+require ("./index.css")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
