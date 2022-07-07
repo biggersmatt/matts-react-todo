296f6e856e60d1d24e0a1104e9f3644fcaaba39f
Matt Biggers <matt.biggers@workday.com>
2022-07-06 17:32:07 -0700

    Updated to react 18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// Dependencies
-import React from 'react';
-import ReactDOM from 'react-dom';
+// // Dependencies
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom"
-import App from './App';
+import App from "./App";
 
 // CSS
 require ("./index.css")
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <App/>
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
